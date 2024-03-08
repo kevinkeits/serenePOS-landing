2068f4e0ca66d6b03cf7e2f7ad13f62e4cd50918
fransiskanauri <124757843+fransiskanauri@users.noreply.github.com>
2024-03-08 15:59:44 +0700

    Update FooterPage.js

--- serenepos-landing/src/elements/FooterPage.js
+++ serenepos-landing/src/elements/FooterPage.js
@@ -4,6 +4,7 @@ const FooterPage = () => {
   return (
     <div>
       <h1>SerenePOS</h1>
+      <p className='text-slate-50'>Copyright © 2024. PT Teman Digital Indonesia. All Rights Reserved.</p>
     </div>
   )
 }
