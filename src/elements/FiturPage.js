0c03087e30891e8eca69d152cc6f33d3edfb981a
fransiskanauri <124757843+fransiskanauri@users.noreply.github.com>
2024-03-13 11:23:34 +0700

    add image

--- src/elements/FiturPage.js
+++ src/elements/FiturPage.js
@@ -7,8 +7,11 @@ const FiturPage = () => {
       <p className='text-4xl m-5 font-bold text-center'>Fitur-fitur Unggulan untuk Aplikasi POS Anda.</p>
       <p className='m-5 mb-28 text-lg text-center'>Buka potensi bisnis Anda dengan aplikasi POS canggih kami.</p>
       <div className='grid grid-cols-1 md:grid-cols-2 items-center m-2 lg:mx-28'>
+        {/* Fitur - Point */}
         <div className='bg-slate-50 px-6 m-5 xl:my-10 md:h-full rounded-lg'>
           <p className='font-semibold text-2xl py-5 text-center'>Antarmuka yang Ramah Pengguna</p>
+          <dotlottie-player src="https://lottie.host/79cd002c-fe8f-49e6-8389-60ec89821090/690ihaoJaO.json" background="transparent" speed="1" style="width: 400px; height: 400px;" loop="" autoplay=""></dotlottie-player>
+          <p className='text-lg'>Antarmuka yang Intuitif dan Mudah Digunakan untuk Navigasi yang Lancar.</p>
         </div>
       </div>
     </div>
