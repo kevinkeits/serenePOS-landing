cb7de335f400cf4957d4696b0574faad8745642d
fransiskanauri <124757843+fransiskanauri@users.noreply.github.com>
2024-03-15 14:21:24 +0700

    update homepage and navbar

--- src/elements/Homepage.js
+++ src/elements/Homepage.js
@@ -15,15 +15,15 @@ const Homepage = () => {
 
   return (
     <div className='relative w-full h-screen'>
-      <video src={homebackground} autoPlay muted loop ref={videoRef} className="absolute top-0 left-0 w-full h-full object-cover blur-sm brightness-110"/>
+      <video src={homebackground} autoPlay muted loop ref={videoRef} className="absolute top-0 left-0 w-full h-full object-cover blur-sm brightness-50"/>
       <div className='absolute inset-0 flex flex-col items-center justify-center text-center text-white'>
       <div className='my-28 lg:px-10 lg:mx-28 '>
-        <h1 className='text-6xl lg:text-8xl py-5 mx-10 mt-10 text-center font-bold text-blue-950'>Serene<span className="text-orange-500">POS</span></h1>
-        <h1 className='text-2xl xl:text-4xl align-middle m-5 py-4 px-2 text-center font-medium text-blue-950'>Optimalkan Bisnismu dengan Kemudahan dan Efisiensi Bersama Aplikasi POS Kami – Transformasi Penjualan Menjadi Pengalaman yang Lebih Baik</h1>
+        <h1 className='text-6xl lg:text-8xl py-5 mx-10 mt-10 text-center font-bold text-white'>SerenePOS</h1>
+        <h1 className='text-2xl xl:text-4xl align-middle m-5 py-4 px-2 text-center font-medium text-white'>Optimalkan Bisnismu dengan Kemudahan dan Efisiensi Bersama Aplikasi POS Kami – Transformasi Penjualan Menjadi Pengalaman yang Lebih Baik</h1>
       </div>
       <div className='flex items-center justify-center'>
-        <h1 className='text-xl m-1 md:m-3 p-2 md:p-5 w-48 md:w-60 border-2 rounded-full text-center border-black bg-blue-950 text-white font-semibold'>Mulai Sekarang</h1>
-        <p className='text-xl m-1 md:m-3 p-2 md:p-5 w-48 md:w-60 text-blue-950 border-2 rounded-full font-semibold bg-white'>Pelajari</p>
+        <p className='text-xl m-1 md:m-3 p-2 md:p-5 w-40 md:w-60 text-white border-2 rounded-full font-semibold hover:bg-white hover:text-blue-600'>Mulai Sekarang</p>
+        <h1 className='text-xl m-1 md:m-3 p-2 md:p-5 w-40 md:w-60 rounded-full text-center font-semibold border-2 border-blue-600 bg-blue-600 hover:bg-white hover:text-blue-600 hover:border-white'>Coba Gratis</h1>
       </div>
       </div>
       
--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
 
   return (
     <div className='fixed z-20 top-0 w-screen'>
-      <div className='flex backdrop-blur-md  bg-black bg-opacity-15 md:justify-center justify-between items-center lg:px-28 lg:py-5 py-3 font-semibold'>
-        <h1 className='text-2xl text-white px-4 mx-2'>SerenePOS</h1>
+      <div className='flex backdrop-blur-md bg-black bg-opacity-15 md:justify-center justify-between items-center lg:px-28 lg:py-5 font-semibold'>
+        <h1 className='text-2xl text-white px-4 mx-2 py-3'>SerenePOS</h1>
         <div className='md:flex md:grow justify-center hidden'>
           <a href="#detailPage"><h1 className='text-lg text-white mx-2 py-2 px-4'>Tentang</h1></a>
           <a href="#fiturPage"><h1 className='text-lg text-white mx-2 py-2 px-4'>Fitur</h1></a>
@@ -22,10 +22,10 @@ const Navbar = () => {
       </div>
       
       {isOpen && ( 
-          <div id="menu-list" className="border-t-2 py-2 border-white backdrop-blur-md bg-black bg-opacity-15 md:hidden">
-              <a href="#detailPage"><p className="hover:cursor-pointer hover:bg-blue-900 py-4 px-5 text-white">Tentang</p></a>
-              <a href="#fiturPage"><p className="hover:cursor-pointer hover:bg-blue-900 py-4 px-5 text-white">Fitur</p></a>
-              <a href="#hargaPage"><p className="hover:cursor-pointer hover:bg-blue-900 py-4 px-5 text-white">Harga</p></a>
+          <div id="menu-list" className="border-t-2 py-2 border-white backdrop-blur-lg bg-black bg-opacity-40 md:hidden">
+              <a href="#detailPage"><p className="hover:cursor-pointer hover:bg-blue-900 py-4 px-5 text-white font-semibold">Tentang</p></a>
+              <a href="#fiturPage"><p className="hover:cursor-pointer hover:bg-blue-900 py-4 px-5 text-white font-semibold">Fitur</p></a>
+              <a href="#hargaPage"><p className="hover:cursor-pointer hover:bg-blue-900 py-4 px-5 text-white font-semibold">Harga</p></a>
           </div>
         )}
     </div>
